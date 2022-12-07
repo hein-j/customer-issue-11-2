18234ea3266ce0ae2f38ceecf9d8467f85b03c7f
Hein Jeong <heinje@amazon.com>
2022-12-07 12:06:50 -0800

    Revert "console log something to trigger build"
    This reverts commit e542f4b9766abf22f51a199d972ab781030b94a7.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log('test')
 root.render(
   <React.StrictMode>
     <App />
